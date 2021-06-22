a7c2cfc40637cf249c5f6aa92b1e18aaf1d8aa3b
chende <641813435@qq.com>
2021-06-22 14:42:18 +0800

    222

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = "com.example.demo")
 public class DemoApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {//
         SpringApplication.run(DemoApplication.class, args);
     }
 
